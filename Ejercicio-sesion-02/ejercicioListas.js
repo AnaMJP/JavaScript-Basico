6fd38b5d361fa2fef178cf65f1993f34fd92ff51
AnaMJP <anaMJP2000@gmail.com>
2023-03-08 12:46:58 +0000

    ejercicio lesion 2

---
+++ Ejercicio-sesion-02/ejercicioListas.js
@@ -0,0 +1,12 @@
+const list = [
+    "Ana MAria",
+    23,
+    true,
+    new Date("may 09 2000"),
+    libro = {
+        titulo: "Bajo la misma estrella",
+        autor: "John Green",
+        "fecha-publicacion": new Date("12 Enero 2012"),
+        url: "https://www.fruugo.es/bajo-la-misma-estrella-la-falla-en-nuestras-estrellas/p-52598870-106245488?language=es&ac=ProductCasterAPI&gclid=Cj0KCQiAgaGgBhC8ARIsAAAyLfHPsR6Tgg6DAhqmWwRT-Ushl71QaZIpk3pxHn6M0E4HsP5pLHGJOAoaAsNMEALw_wcB"
+    }
+]
